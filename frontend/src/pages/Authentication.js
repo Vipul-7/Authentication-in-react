08a9b59a151136f9f32a0973fe2c6b76a4faa492
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-21 23:17:16 +0530

    Attaching Auth Tokens to Outgoing Requests

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -10,15 +10,15 @@ export default AuthenticationPage;
 export const action = async ({ request, params }) => {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
-  
+
   if (mode !== "login" && mode !== "signup") {
     throw json({ message: "Unsupported mode" }, { status: 422 });
   }
 
   const data = await request.formData();
   const authData = {
-    email: data.get('email'),
-    password: data.get('password'),
+    email: data.get("email"),
+    password: data.get("password"),
   };
 
   const response = await fetch("http://localhost:8080/" + mode, {
@@ -36,6 +36,11 @@ export const action = async ({ request, params }) => {
   if (!response.ok) {
     throw json({ message: "Could not authenticate user" }, { status: 500 });
   }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
 
   return redirect("/");
 };
