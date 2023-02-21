48b153f46027aad38f402da248db77cecae51f6e
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-22 00:47:40 +0530

    Managing the Token Expiration & Automatic logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,32 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  // for token expires after 1 hour we should log the user out
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
